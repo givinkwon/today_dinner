a1c2c048cca65ec6c3ebd07c87db97f11236d2d5
givinkwon <rlqls505@naver.com>
2022-01-18 17:08:36 +0900

    리팩토링
    - 고민 중 :  Activity Logic의 경우 DB와는 별개로 UI에 관련된 state data만을 (android viewmodel와 약간 유사하면서도 다름. viewmodel에서는 DB 호출은 연동되지만, 지금 구조는 DB 호출까지 완전 분리해버림)  관리하는 것은 어떨까? state가 줄어들수록 반복 렌더링의 비용이 줄기 때문에 불필요한 자원 낭비를 막을 수 있을듯.
    
    솔직히 고민하는 부분이.. DB 호출을 연결하고 UI부분에서 좀 더 편하게 쓰냐 VS DB 호출 연결을 막아서 보일러 플레이트 코트를 없애냐

--- lib/providers/activity/Home.dart
+++ lib/providers/activity/Home.dart
@@ -19,19 +19,6 @@ class Home with ChangeNotifier {
   // 필터 태그 선택 시
   void add_filter(value) {
     filter_list.add(value);
-
-    if (top_index == 1) {
-      // 데이터 호출
-      Feed().get_data(Filter: filter_list);
-    }
-    if (top_index == 2) {
-      // 데이터 호출
-      Freetalk().get_data(Filter: filter_list);
-    }
-    if (top_index == 3) {
-      // 데이터 호출
-      Recipe().get_data(Filter: filter_list);
-    }
     // 구독 widget에게 변화 알려서 re-build
     notifyListeners();
   }
@@ -49,47 +36,6 @@ class Home with ChangeNotifier {
     // 필터 초기화
     filter_list = [];
 
-    // 구독 widget에게 변화 알려서 re-build
-    notifyListeners();
-  }
-
-  // 좋아요 & 북마크 추가
-  void add_like(GoalField, DocId, UserEmail) {
-    // Feed
-    if (top_index == 1) {
-      Feed().update_data(DocId, GoalField, UserEmail);
-    }
-
-    // Freetalk
-    if (top_index == 2) {
-      Freetalk().update_data(DocId, GoalField, UserEmail);
-    }
-
-    // Recipe
-    if (top_index == 3) {
-      Recipe().update_data(DocId, GoalField, UserEmail);
-    }
-    // 구독 widget에게 변화 알려서 re-build
-    notifyListeners();
-  }
-
-  // 좋아요 제거
-  void remove_like(GoalField, DocId, UserEmail) {
-    // Feed
-    if (top_index == 1) {
-      Feed().delete_data(DocId, 'field', Field: GoalField, Value: UserEmail);
-    }
-
-    // Freetalk
-    if (top_index == 2) {
-      Freetalk()
-          .delete_data(DocId, 'field', Field: GoalField, Value: UserEmail);
-    }
-
-    // Recipe
-    if (top_index == 3) {
-      Recipe().delete_data(DocId, 'field', Field: GoalField, Value: UserEmail);
-    }
     // 구독 widget에게 변화 알려서 re-build
     notifyListeners();
   }
