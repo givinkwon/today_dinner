9cc08fc5431d794b26036b6cbeb51070e81379b0
eastsweat <rlqls505@naver.com>
2022-02-04 20:00:11 +0900

    기능 수정 : 결과 이미지 수정
    일부 기기 짤림

--- lib/utils/NoResult.dart
+++ lib/utils/NoResult.dart
@@ -4,7 +4,7 @@ Widget NoResult(BuildContext context) {
   {
     return Container(
         color: Color.fromRGBO(255, 255, 255, 1),
-        width: MediaQuery.of(context).size.width * 0.85,
+        width: MediaQuery.of(context).size.width * 0.8,
         child: Image.asset(
           'assets/result_not.png',
         ));
